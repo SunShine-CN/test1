f62c0387794d6832227c83e53ab7b376a68762c8
SunShine-CN <2257045651@qq.com>
2020-06-29 10:12:38 +0800

    master

--- test.cpp
+++ test.cpp
@@ -9,5 +9,19 @@ int main(void)
     cout << "hello world" << endl;
     printf("sunjun is good!\n");
     scanf("%d",&a);
+    printf("u");
     return 0;
 }
+
+/*
+git init    初始化本地仓库
+git config --global user.name   配置用户名
+git config --global user.email  配置邮箱
+git remote add origin code@github.com   绑定本地和远程仓库
+git pull    拉取远程仓库的变化同步本地的状态
+git add     确认本地仓库的变化到本地缓存区
+git commit  确认本地缓存区的内容，可以准备push
+git push    提交本地仓库到远程仓库
+git status  确认本地缓存区的状态
+
+*/
