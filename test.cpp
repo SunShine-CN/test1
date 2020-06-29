62e36360619417901bfa36c586420d67817cf35b
SunShine-CN <2257045651@qq.com>
2020-06-29 09:21:42 +0800

    master

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(void)
+{
+    int a;
+    cout << "hello world" << endl;
+    printf("sunjun is good!\n");
+    scanf("%d",&a);
+    return 0;
+}
