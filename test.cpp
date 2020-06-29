b47fec50c8b9abb42577bfdaedd78a129f51437a
SunShine-CN <2257045651@qq.com>
2020-06-29 09:27:39 +0800

    master

--- test.cpp
+++ test.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(void)
 {
     int a;
-    cout << "hello world" << endl;
+    //cout << "hello world" << endl;
     printf("sunjun is good!\n");
     scanf("%d",&a);
     return 0;
